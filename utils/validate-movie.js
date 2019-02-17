a977c193b5cde719642ecec24d92b18e20e54a84
Aaron Shivers <aaron@moonmail.org>
2019-02-17 08:06:35 -0600

    added objectId validation

--- utils/validate-movie.js
+++ utils/validate-movie.js
@@ -1,9 +1,10 @@
 const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi)
 
 const validateMovie = movie => {
   const schema = {
     title: Joi.string().min(3).max(50).required(),
-    genreId: Joi.string().min(3).max(30).required(),
+    genreId: Joi.objectId().required(),
     numberInStock: Joi.number().min(0).max(255).required(),
     dailyRentalRate: Joi.number().min(0).max(255).required()
   }
--- utils/validate-rental.js
+++ utils/validate-rental.js
@@ -1,9 +1,10 @@
 const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi)
 
 const validateRental = rental => {
   const schema = {
-    movieId: Joi.string().required(),
-    customerId: Joi.string().required()
+    movieId: Joi.objectId().required(),
+    customerId: Joi.objectId().required()
   }
   return Joi.validate(rental, schema)
 }
